5cb68003d1aed24aa625ef945bf25640d8355b3a
gitsryn <128663334+gitsryn@users.noreply.github.com>
2023-04-01 14:39:00 -0700

    Revert "committed"
    This reverts commit 34709f6c588584dfceffe2cc71105307b59feb93.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -14,9 +14,9 @@ export default async function (req, res) {
     });
     return;
   }
-
-  const activities = req.body.animal || '';
-  if (activities.trim().length === 0) {
+=======
+const animal = req.body.animal || '';
+if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
         message: "Please enter a valid animal",
@@ -51,10 +51,16 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three animals for me based on my interest.
+  return `Suggest three names for an animal that is a superhero.
 
 Interest: Animal
 Names: Mt Si, Agent Fluffball, The Incredible Feline
+=======
+  return `Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+>>>>>>> parent of 34709f6 (committed)
 Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: ${capitalizedAnimal}
